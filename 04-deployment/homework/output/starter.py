76666550cbd084e1a09e010440d7ff81e93a9069
Chiragks-1 <82097189+Chiragks-1@users.noreply.github.com>
2024-07-02 06:58:34 +0000

    executed and completed the quiz5 of module 4.

--- 04-deployment/homework/output/starter.py
+++ 04-deployment/homework/output/starter.py
@@ -1,33 +1,35 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[19]:
 
 
-get_ipython().system('pip freeze | grep scikit-learn')
+#get_ipython().system('pip freeze | grep scikit-learn')
 
 
-# In[2]:
+# In[20]:
 
 
-get_ipython().system('python -V')
+#get_ipython().system('python -V')
 
 
-# In[3]:
+# In[21]:
 
 
 import pickle
 import pandas as pd
+import sys
 
 
-# In[5]:
+
+# In[22]:
 
 
 with open('model.bin', 'rb') as f_in:
     dv, model = pickle.load(f_in)
 
 
-# In[6]:
+# In[23]:
 
 
 categorical = ['PULocationID', 'DOLocationID']
@@ -45,32 +47,42 @@ def read_data(filename):
     return df
 
 
+# In[18]:
+
+
+year = int(sys.argv[1])
+month = int(sys.argv[2])
+
+input_file = f'https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{year:04d}-{month:02d}.parquet'
+output_file = f'output/yellow_tripdata_{year:04d}-{month:02d}.parquet'
+
+
 # In[7]:
 
 
-df = read_data('https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2023-03.parquet')
-
+#pip install pyarrow
 
 
 # In[8]:
 
 
-pip install pyarrow
+#pip install --upgrade pip
 
 
 # In[9]:
 
 
-df = read_data('https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2023-03.parquet')
+df = read_data(input_file)
+df['ride_id'] = f'{year:04d}/{month:02d}_' + df.index.astype('str')
 
 
-# In[14]:
+# In[10]:
 
 
 df.head()
 
 
-# In[10]:
+# In[11]:
 
 
 dicts = df[categorical].to_dict(orient='records')
@@ -82,94 +94,42 @@ y_pred = model.predict(X_val)
 
 
 print(y_pred)
+print('mean predicted duration:',y_pred.mean())
 
 
 # In[13]:
 
 
-std_dev = y_pred.std()
-print(std_dev)
+#std_dev = y_pred.std()
+#print(std_dev)
+
+
+# In[14]:
+
+
+#df_result = pd.DataFrame()
+#df_result['ride_id'] = df['ride_id']
 
 
 # In[15]:
 
 
-year = 2023
-month = 3
-
-input_file = f'https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{year:04d}-{month:02d}.parquet'
-df = read_data(input_file)
-
-
-# In[21]:
-
-
-output_file = f'output/yellow_tripdata_{year:04d}-{month:02d}.parquet'
+#df_result['predicted_duration'] = y_pred
 
 
 # In[16]:
 
 
-df['ride_id'] = f'{year:04d}/{month:02d}_' + df.index.astype('str')
+#df_result.to_parquet(
+    #output_file,
+    #engine='pyarrow',
+    #compression=None,
+    #index=False
+#)
 
 
 # In[17]:
 
 
-df.head()
-
-
-# In[18]:
-
-
-df_result = pd.DataFrame()
-df_result['ride_id'] = df['ride_id']
-
-
-# In[19]:
-
-
-df_result['predicted_duration'] = y_pred
-
-
-# In[23]:
-
-
-df_result.to_parquet(
-    output_file,
-    engine='pyarrow',
-    compression=None,
-    index=False
-)
-
-
-# In[28]:
-
-
-get_ipython().system('ls -ltrh output/')
-
-
-# In[29]:
-
-
-pip install nbconvert
-
-
-# In[30]:
-
-
-pip install --upgrade pip
-
-
-# In[31]:
-
-
-pip install nbconvert
-
-
-# In[44]:
-
-
-#cd /workspaces/mlops-zoomcamp0o4-deployment/homework/output
-jupyter nbconvert --to script --output-dir=/workspaces/mlops-zoomcamp/0o4-deployment/homework/output starter.ipynb
+#get_ipython().system('ls -ltrh output/')
 
